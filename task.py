d6fe001fe4c9b5e80d493dcb5e42a3747c83d858
Jack <30070299@lcwc.ac.uk>
2024-09-17 18:37:14 +0000

    you better be right

--- task.py
+++ task.py
@@ -1,21 +1,42 @@
 def task_1(): # Lists
-
+    print("what your name: ")
+    name = input()
     origional_list = ["Geoff", "Jeff", "Jeffrey"]
+    origional_list.insert(0,name)
+    origional_list.pop(2)
+    new_list = origional_list.copy()
 
     return new_list
 
 
 def task_2(): # Dictionaries
 
-    keys = ("name", "age", "profession")
-    values = ("Geoff", 35, "technician")
+    keys = ("name", "age", "profession", "car")
+    values = ("Geoff", 35, "technician", "car")
+    geoff = dict(zip(keys, values))
+    
+    key = ("make", "model", "engine", "colour")
+    val = ("ford", "focus", 1.6, "blue")
+    car = dict(zip(key, val))
+    
+    person = {
+        "geoff" : geoff,
+        "car" : car,
+    }
 
     return person
 
 
 def task_3(): # Tuples
+    print("what is your name: ")
+    name = input()
+    print("what is the subject you do: ")
+    subject = input()
+    print("What was your score: ")
+    score = int(input())
     student_1 = ("Geoff", "Maths", 80)
-    student_2 = ()
+    student_2 = (name, subject, score)
+    students = student_1 + student_2
 
     return students
 
@@ -23,9 +44,13 @@ def task_3(): # Tuples
 def task_4(): # Sets
     fruits_1 = {"apple", "banana", "cherry", "grape", "mango", "pineapple", "papaya","sprite", "orange", "lemon", "strawberry"}
     fruits_2 = {"raspberry", "banana", "cherry", "grape", "mango", "blueberry", "papaya", "melon", "lemon", "steak"}
+    fruits_1.remove("sprite")
+    fruits_2.remove("steak")
+    symmetric_difference = fruits_1.symmetric_difference(fruits_2)
+    intersection = fruits_1.intersection(fruits_2)
 
-    duplicate_fruits = None # This should be a tuple containing all the fruits in both tuples
-    individual_fruits = None # This should be a tuple containing only the individual fruits
+    duplicate_fruits = intersection # This should be a tuple containing all the fruits in both tuples
+    individual_fruits = symmetric_difference # This should be a tuple containing only the individual fruits
 
 
     return [duplicate_fruits, individual_fruits] # Note - functions can only return one data item - so both tuples
